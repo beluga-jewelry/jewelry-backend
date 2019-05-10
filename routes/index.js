1f24633b4d927e82bcc9b5017f636fffc1de5962
darmonlyonelab <manusporn.f@ku.th>
2019-05-10 18:15:58 +0700

    promotion api

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var mongodb = require("mongodb");
-
+var ObjectId = require("mongodb").ObjectID;
 module.exports = router;
 
 // router.get('/', async (req,res) => {
@@ -13,11 +13,13 @@ module.exports = router;
 //   res.send(mimint);
 // });
 
-// router.post('/push',async (req,res) => {
-//   const postCollections = await loadCollections("store")
-//   await postCollections.insertMany([])
-//   res.send("finish");
-// });
+router.post('/push',async (req,res) => {
+  const postCollections = await loadCollections("promotion_dim")
+  await postCollections.updateOne({promotion_name: "1 get free 1"},{$set:{_id:1}})
+  await postCollections.updateOne({promotion_name:"midyear sale"},{$set:{_id:2}})
+  await postCollections.updateOne({promotion_name:"year end sale"},{$set:{_id:3}}) 
+  res.send(await postCollections.find({}));
+});
 
 router.post('/date/:stock/:outstock', async (req,res) => {
   const postCollections = await loadCollections("date_dim")
@@ -103,11 +105,77 @@ for (let i = 0; i < len; i++) {
   res.send(product)  
 });
 
+router.get('/promotion/store', async (req,res) => {
+  const db = await loadDataBase()
+  const productCollections = await db.collection("product_dim")
+  const storeCollections = await db.collection("store")
+  const promoCollections = await db.collection("promotion_dim")
+
+  var promotionData = await promoCollections.find({ 
+    begin_date:{
+      $lte: new Date()
+    },
+    end_date: { 
+      $gte: new Date()
+    }
+  }).toArray()
+
+
+  var promoTemp = [];
+var len = promotionData.length
+for (let i = 0; i < len; i++) {
+  promoTemp[i] = promotionData[i]._id
+}
+var store = await storeCollections.find({promotion_id: { $in: promoTemp }}).project({product_id:1}).toArray()
+var store2 = await storeCollections.find({promotion_id: { $in: promoTemp }}).toArray()
+
+var storeTemp = [];
+var promoId = []
+var len2 = store.length
+for (let i = 0; i < len2; i++) {
+  storeTemp[i] = store[i].product_id
+}
+var product = await productCollections.find({_id: {$in: storeTemp}}).toArray()
+
+var sendArray = []
+var len3 = product.length
+let i = 0, k = 0;
+while(k < len3){
+  if(store[i].product_id == product[k]._id){
+    promoId[k] = store2[i]['promotion_id']
+    var promoSet = await promoCollections.findOne({_id:ObjectId(promoId[k])});
+  sendArray[k] = { "_id": product[i]['id'],
+  "name": product[k]['name'],
+  "type": product[k]['type'],
+  "material": product[k]['material'],
+  "gender": product[k]['gender'],
+  "color": product[k]['color'],
+  "price": product[k]['price'],
+  "image":product[k]['image'],
+  "promotion_name": promoSet['promotion_name'],
+  "quantity":promoSet['quantity'],
+  "discount":promoSet['discount'],
+  }  
+    k++;
+    }
+  i++;
+  if(i >= len2) i = 0;
+}
+
+res.send(sendArray)  
+})
+
+/*
+*load collection 
+*/
 async function loadCollections(collection) {
   var db = await loadDataBase();
   return db.collection(collection);
 }
 
+/**
+ * load dataBase
+ */
 async function loadDataBase() {
   const client = await mongodb.MongoClient.connect(
     "mongodb+srv://darm:007007@cluster0-umqz2.gcp.mongodb.net/test?retryWrites=true",{ useNewUrlParser: true }
